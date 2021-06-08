b90f7a40ca8c76af5999092c4b4cf40aa553a6d4
Robin Visser <robin.visser@hva.nl>
2021-06-08 19:51:05 +0200

    Update main.py

---
+++ SSH-Logger/main.py
@@ -0,0 +1,17 @@
+##################################################
+# SSH Authentication Analyzer
+##################################################
+# Author: Robin Visser
+# Version: 1.0 (Beta)
+# Email: robinvisser92@live.nl
+# Copyright: 2021
+##################################################
+
+import time
+
+# Starting time
+start = time.time()
+
+# Ending time
+end = time.time()
+
